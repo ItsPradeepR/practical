25469f8cd65cad2b30080114b77277c780d21a11
ItsPradeepR <pradeepkrajput@outlook.com>
2021-11-01 23:57:47 +0530

    Commit Practical theme

---
+++ app/design/frontend/Interview/practice/web/requirejs-config.js
@@ -0,0 +1,15 @@
+var config = {
+    deps: [
+        "js/cstm-script"
+      ],
+    paths: {            
+            'slick': "js/slick.min"
+            
+        },   
+    shim: {
+        'slick': {
+            deps: ['jquery']
+        }
+    }
+    
+};
---
+++ app/etc/env.php
@@ -0,0 +1,83 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => 'aa10fbefef3e8c4866ba92d799588c9f'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'salecto',
+                'username' => 'root',
+                'password' => '',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '02c_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '02c_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => false
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        '127.0.0.1'
+    ],
+    'install' => [
+        'date' => 'Fri, 29 Oct 2021 06:02:48 +0000'
+    ]
+];
